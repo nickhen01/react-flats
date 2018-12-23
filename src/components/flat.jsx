e6f37896c497a28e3a38f5e85f936a2719a35415
Nicolas Hennick <nicolashennick@gmail.com>
2018-12-23 14:02:14 +0100

    added stylesheet, app, flat and flat-list files

---
+++ src/components/flat.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Flat extends Component {
+
+
+}
+
+
+export default Flat;
---
+++ src/components/flat_list.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Flat from './flat.jsx'
+
+const FlatList = (props) => {
+  return (
+    <div className="flat-list">
+      {}
+    </div>
+  );
+};
+export default FlatList;
