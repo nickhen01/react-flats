381c13bb8f0662a55a73fd834fed79f7293623e1
Nicolas Hennick <nicolashennick@gmail.com>
2018-12-23 16:24:12 +0100

    flat list completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+import FlatList from './flat_list.jsx'
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <FlatList />
+      </div>
+    )
+  }
+}
 
 export default App;
